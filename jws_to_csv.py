7d6e6e4d66861aec01e8ce2a3b274d5486cc6d8a
odoluca <osman.doluca@iue.edu.tr>
2019-01-28 20:38:37 +0300

    working code

--- jws_to_csv.py
+++ jws_to_csv.py
@@ -2,10 +2,18 @@ import io
 import OleFileIO_PL as ofio
 from struct import unpack
 
+def data_definitions(x):
+    return {
+        268435715:"WAVELENGTH",
+        4097:"CD",
+        8193:"HT VOLTAGE",
+        3:"ABSORBANCE",
+        14:"FLUORESCENCE"
+    }.get(x,'undefined')
 
 class JwsHeader:
     def __init__(self, channel_number, point_number,
-                 x_for_first_point, x_for_last_point, x_increment,
+                 x_for_first_point, x_for_last_point, x_increment,header_names,
                  data_size=None):
         self.channel_number = channel_number
         self.point_number = point_number
@@ -14,38 +22,46 @@ class JwsHeader:
         self.x_increment = x_increment
         #only defined if this is the header of a v1.5 jws file
         self.data_size = data_size
+        self.header_names=header_names
 
 
-def _unpack_ole_jws_header(data,format):
-    # if len(data) < 96:
-    #     raise _NotEnoughDataError, "DataInfo should be at least 96 bytes!"
+def _unpack_ole_jws_header(data):
+    try:
+        data_tuple = unpack('<LLLLLLddd', data[0:48])
+        # print(data_tuple)
+        channels=data_tuple[3]
+        nxtfmt='<L'+'L'*channels
+        header_names = list(unpack(nxtfmt, data[48:48+4*(channels+1)]))
 
-    # data_tuple = unpack(DATAINFO_FMT, data)
-    data_tuple = unpack('<LLLLdddd', data[0:48])
-    print(data_tuple)
-    channels=data_tuple[3]
-    nxtfmt='<L'+'L'*channels
-    data_tuple = data_tuple+unpack(nxtfmt, data[48:48+4*(channels+1)])
-    print(data_tuple)
-    lastPos=48+4*(channels+1)
-    nxtfmt='<LLdddd'
-    for pos in range(channels):
-        data_tuple = data_tuple + unpack(nxtfmt, data[lastPos:lastPos+40])
-        print(data_tuple)
+        for i,e in enumerate(header_names):
+            header_names[i]=data_definitions(e)
 
-    # print(data_tuple[9:13])
-    # I have only found spectra files with 1 channel
-    # So we will only support 1 channel per file, at the moment
-    # return JwsHeader(data_tuple[3], data_tuple[5],
-    #                  data_tuple[6], data_tuple[7], data_tuple[8])
+
+        data_tuple+=tuple(header_names)
+        # print(header_names)
+        # print(data_tuple)
+
+        lastPos=48+4*(channels+1)
+        nxtfmt='<LLdddd'
+        for pos in range(channels):
+            data_tuple = data_tuple + unpack(nxtfmt, data[lastPos:lastPos+40])
+            # print(data_tuple)
+
+        # print(data_tuple[9:13])
+        # I have only found spectra files with 1 channel
+        # So we will only support 1 channel per file, at the moment
+        return JwsHeader(data_tuple[3], data_tuple[5],
+                         data_tuple[6], data_tuple[7], data_tuple[8],header_names)
+    except:
+        exit("Cannot read DataInfo")
 
 file4="sample_CD_HT_Abs.jws"
 file5="sample_CD_HT_Abs_2.jws"
-DATAINFO_FMT = '<LLLLLLdddLLLLddddLLLLLLdddLLLLddd'
-DATAINFO_FMT = '<LLLLLLdddLLLLLLdddLLLLLLdddLLdddd'
-DATAINFO_FMT = '<LLLLLLdddLLLLLLddddLLddddLLdddd'
-"""                 ^ ^^^^      ^^^^  ^^^^  ^^^^     """
-#268435715 is separator. after first seperator comes the headers. Then each line comes the header.
+# DATAINFO_FMT = '<LLLLLLdddLLLLddddLLLLLLdddLLLLddd'
+# DATAINFO_FMT = '<LLLLLLdddLLLLLLdddLLLLLLdddLLdddd'
+# DATAINFO_FMT = '<LLLLLLdddLLLLLLddddLLddddLLdddd'
+# """                 ^ ^^^^      ^^^^  ^^^^  ^^^^     """
+#268435715 is separator or "wavelength" symbol. after first seperator comes the headers. Then each line comes the header.
 
 file1="sample_fluorescence.jws"
 file2="sample_fluorescence_2.jws"
@@ -53,22 +69,39 @@ file3="sample_fluorescence_3.jws"
 # DATAINFO_FMT = '<LLLLLLdddLLLLdddd'
 
 
-def readheader(filename,format):
-    f=open(filename,"rb")
-    print(f.read(4))
+def convert_jws_to_csv(filename):
+    with open(filename,"rb") as f:
+        # print(f.read(4))
 
-    # print(file.read(100).decode('utf-8'))
-    # f=io.StringIO(file)
-    f.seek(0)
-    oleobj=ofio.OleFileIO(f)
-    str = oleobj.openstream('DataInfo')
-    header_data = str.read()
-    header_obj = _unpack_ole_jws_header(header_data,format)
-    print(header_obj.point_number,header_obj.channel_number)
+        f.seek(0)
+        oleobj=ofio.OleFileIO(f)
+        data = oleobj.openstream('DataInfo')
+        header_data = data.read()
+        header_obj = _unpack_ole_jws_header(header_data)
+        # print(header_obj.point_number,header_obj.channel_number)
+        # print(oleobj.openstream('Y-Data').read())
+        fmt = 'f' * header_obj.point_number*header_obj.channel_number
+        values = unpack(fmt, oleobj.openstream('Y-Data').read())
+        chunks = [values[x:x + header_obj.point_number] for x in xrange(0, len(values), header_obj.point_number)]
 
+        # print("len: %i*%i, %s" %(header_obj.point_number,header_obj.channel_number,chunks))
+    with open(filename.rstrip("jws")+"csv","w") as r:
+        print("file name: %s\t header names: %s" %(filename,header_obj.header_names))
+        r.write("sep=,\n")
+        r.write( ",".join(str(x) for x in header_obj.header_names))
+        r.write("\n")
+        for line_no in range(header_obj.point_number):
+            r.write(str(header_obj.x_for_first_point+line_no*header_obj.x_increment))
+            r.write(",")
+            for c in range(header_obj.channel_number):
+                r.write(str(chunks[c][line_no]))
+                r.write(",")
+            r.write('\n')
+        print("%s is created." %r.name)
 
-readheader(file1,DATAINFO_FMT)
-readheader(file2,DATAINFO_FMT)
-readheader(file3,DATAINFO_FMT)
-# readheader(file4,DATAINFO_FMT)
-# readheader(file5,DATAINFO_FMT)
+convert_jws_to_csv(file1)
+convert_jws_to_csv(file2)
+convert_jws_to_csv(file3)
+convert_jws_to_csv(file4)
+convert_jws_to_csv(file5)
+
